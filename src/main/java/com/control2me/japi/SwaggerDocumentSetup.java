8253fd9dcc1cc489c15dff538e229d24e8e65315
Justin <justin.mulcahy@gmail.com>
2018-12-06 16:59:26 +0000

    Initial commit

---
+++ src/main/java/com/control2me/japi/SwaggerDocumentSetup.java
@@ -0,0 +1,28 @@
+package com.control2me.japi;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import io.swagger.jaxrs.config.BeanConfig;
+
+public class SwaggerDocumentSetup extends HttpServlet{
+	
+	private static final long serialVersionedID = 1L;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException{
+		super.init(config);
+		BeanConfig beanConfig = new BeanConfig();
+		beanConfig.setVersion("1.0.0");
+		beanConfig.setTitle("Triskel General API's");
+		beanConfig.setDescription("Core API's");
+		beanConfig.setSchemes(new String[]{"http"});
+		beanConfig.setHost("localhost:8080");
+		beanConfig.setBasePath("/japi/services");
+		beanConfig.setResourcePackage("com.control2me.japi.services");
+		beanConfig.setScan(true);
+		
+	}
+
+}
